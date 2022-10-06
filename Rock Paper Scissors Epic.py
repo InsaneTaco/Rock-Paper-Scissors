e4be4dff37b3b42296c34ff7050cbdb8e6db3e3a
mitya <88312178+MityaWasTaken@users.noreply.github.com>
2022-10-06 15:50:52 -0700

    Update Rock Paper Scissors Epic.py

--- Rock Paper Scissors Epic.py
+++ Rock Paper Scissors Epic.py
@@ -1,28 +1,30 @@
-while True:
-    import random
-    move = input('Welcome to Epic Rock Paper Scissors! Make your move. Type your move in all lowercase. ')
-    oppnumber = random.randint(1,3)
-    if oppnumber == 1:
-        oppmove = 'rock'
-    elif oppnumber == 2:
-        oppmove = 'paper'
-    elif oppnumber == 3:
-        oppmove = 'scissors'
-    if move == 'rock' and oppmove == 'rock':
-        print('Opponent chose rock. Draw.')
-    elif move == 'rock' and oppmove == 'scissors':
-        print('Opponent chose scissors. You won!')
-    elif move == 'rock' and oppmove == 'paper':
-        print('Opponent chose paper. Defeat.')
-    elif move == 'paper' and oppmove == 'paper':
-        print('Opponent chose paper. Draw.')
-    elif move == 'paper' and oppmove == 'rock':
-        print('Opponent chose rock. You won!')
-    elif move == 'paper' and oppmove == 'scissors':
-        print('Opponent chose scissors. Defeat.')
-    elif move == 'scissors' and oppmove == 'scissors':
-        print('Opponent chose scissors. Draw.')
-    elif move == 'scissors' and oppmove == 'paper':
-        print('Opponent chose paper. You won!')
-    elif move == 'scissors' and oppmove == 'rock':
-        print('Opponent chose rock. Defeat.')
+import random
+
+class RockPaperScissors:
+    def __init__(self, playerMove):
+        self.playerMove = playerMove
+        self.opponentMove = ''
+
+    def rockPaperScissors(self):
+        __possibleMoves = ["scissors", 'rock', 'paper']
+
+        for i in range(random.randint(1, 3)):
+            self.opponentMove = __possibleMoves[i]
+
+        if playerMove != None:
+            if self.opponentMove == self.playerMove:
+                return "draw"
+
+            elif self.playerMove == 'scissors' and self.opponentMove == 'paper':
+                return "win"
+            elif self.playerMove == 'paper' and self.opponentMove == 'rock':
+                return "win"
+            elif self.playerMove == 'rock' and self.opponentMove == 'scissors':
+                return "win"
+            else:
+                return "loose"
+
+playerMove = input('Welcome to Epic Rock Paper Scissors! ').lower()
+rps = RockPaperScissors(playerMove)
+
+print(rps.rockPaperScissors())
